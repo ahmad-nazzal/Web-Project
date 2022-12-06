1e25b3b0ee58637f6575a067c036357b43657719
SALAHT4N <salah.aldin.tanbour@outlook.com>
2022-12-07 00:29:00 +0200

    search results page almost done

--- html/search-results.php
+++ html/search-results.php
@@ -1,3 +1,15 @@
+<?php
+  $flag = false;
+  $user_input = 0;
+  if(isset($_GET['search-field']) &&  !empty($_GET['search-field']))
+  {
+    $flag = true;
+    $user_input = $_GET['search-field'];
+  }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
@@ -19,103 +31,187 @@
   
     <script defer src="../js/test.js"></script>
     <link rel="stylesheet" href="../css/search-results.css">
+    <link rel="stylesheet" href="../css/general.css">
 </head>
 <body>
-  <nav>
-    Nav
-  </nav>
+<header>
+        <nav class="navbar navbar-dark navbar-expand-sm">
+          <div class="container-fluid">
+            <a class="navbar-brand logo" href="#"><h3>آجار</h3></a>
+            <form action="search-results.php" method="get" class="form-search">
+              <input
+                class="form-control search-bar"
+                type="text"
+                placeholder="بحث..."
+                name="search-field"
+              />
+              <input type="submit" class="search-btn btn" title="بحث" />
+            </form>
+            <ul class="navbar-nav gap-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">الصفحة الرئيسية</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">الصفحة الرئيسية </a>
+              </li>
+              <li class="nav-item">
+                <i
+                  class="bi-person-fill icons"
+                  data-bs-toggle="modal"
+                  data-bs-target="#sign-in-modal"
+                  ><span class="fs-6">تسجيل الدخول</span>
+                </i>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
   <main class="container mt-5">
     <div class="row">
 
-      <section class="col-3 h-100 mt-1">
-      <h4>Filters</h4>
-        <div class="card sticky-top">
-          <div class="card-body">
-            <div class="card-body">
+        <section class="col-3 h-100 mt-2">
+          <h5>فلترة النتائج</h5>
+            <div class="card sticky-top">
+              <div class="card-body">
+                <div class="card-body">
 
-              <h5 class="card-text">Shipping options</h5>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                  Local pickup
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                <label class="form-check-label" for="flexCheckChecked">
-                  Checked checkbox
-                </label>
-              </div>
+                  <h5 class="card-text">خيارات التوصيل</h5>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      الاستلام الذاتي
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                      توصيل
+                    </label>
+                  </div>
+                </div>
+                <div class="card-body">
+
+                  <h5 class="card-text">طرق الدفع</h5>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      كاش
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                      بطقاة ائتمان
+                    </label>
+                  </div>
+                  
             </div>
             <div class="card-body">
 
-              <h5 class="card-text">Payment methods</h5>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                  Cash
-                </label>
+                  <h5 class="card-text">الموقع</h5>
+                  <div>
+                    <label for="exampleDataList" class="form-label">المدينة</label>
+                    <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="اكتب المدينة ...">
+                    <datalist id="datalistOptions">
+                      <option value="Nablus">
+                      <option value="Jenin">
+                      <option value="Tolkarem">
+                      <option value="Qalqilya">
+                      <option value="Hebron">
+                    </datalist>
+                  </div>
               </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                <label class="form-check-label" for="flexCheckChecked">
-                  credit card
-                </label>
-              </div>
-              
-        </div>
-        <div class="card-body">
+              <div class="d-flex justify-content-end">
 
-              <h5 class="card-text">Location</h5>
-              <div class="form-check">
-                <label for="exampleDataList" class="form-label">City</label>
-                <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-                <datalist id="datalistOptions">
-                  <option value="Nablus">
-                  <option value="Jenin">
-                  <option value="Tolkarem">
-                  <option value="Qalqilya">
-                  <option value="Hebron">
-                </datalist>
+                <button class="btn btn-primary justify-content-end">افعل</button>
               </div>
-          </div>
-          <div class="d-flex justify-content-end">
-
-            <button class="btn btn-dark justify-content-end">filter</button>
-          </div>
-      </section>
+        </section>
       <section class="col-9">
-      <h3>Results</h3>
+      <h3>النتائج</h3>
+      <?php
+      if ($flag == true)
+      {
+        getCards($user_input);
+
+      }
+      function getCards($user_input)
+      {
+        $db = new mysqli("localhost", "root", "", "web_project_agar");
+        if (isset($db))
+        {
+
+          $query = 
+          ' 
+          select title, price_per_day as price, rate.stars, description, shipping_cost, location
+          from items
+          inner join (select avg(rating) as stars, item_id from reviews) rate on items.id = rate.item_id
+          
+          where items.title like "%'.$user_input.'%" or items.description like "%'.$user_input.'%";';
+
+          $result = $db->query($query);
+          
+
+          for ($i = 0; $i < $result->num_rows; $i++)
+          {
+            $row = $result->fetch_object();
+            ?>
+         
+        
+      
         <div class="card mb-2 ">
           <div class="row">
             <div class="col-auto">
-              <img src="https://picsum.photos/200" alt="">
+              <img src="https://picsum.photos/200" alt="" class="rounded-start">
             </div>
             <div class="col-9 ">
               <div class="card-body">
                 <div class="item-info">
                   <div>
-                    <h4 class="card-title">Lorem ipsum dolor sit amet.</h4>
+                    <h4 class="card-title"><?php echo $row->title ?></h4>
+                    
                     <div class="d-flex star-container mb-2">
+                    <?php 
+                      for($i = 0; $i < $row->stars -1; $i++)
+                      {
+                        ?>
+
+                      
                     <i class="bi bi-star-fill"></i>
+                    <?php 
+                      }
+                    
+                      for($i = 0; $i < 5 - $row->stars; $i++)
+                      {
+
+                  ?>
+<!--                     
                     <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i> -->
                     <i class="bi bi-star"></i>
-                    <i class="bi bi-star"></i>
-                
+                  <?php 
+                      }
+                  ?> 
+                    <!-- <i class="bi bi-star"></i> -->
                 </div>
                   </div>
-                  <p class="price"><strong>0.99$</strong></p>
+                  <p class="price"><strong><?php echo $row->price ?>$</strong></p>
                 </div>
                 
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi illum quo, fuga aut rem saepe?</p>
-                <p class="card-text"><small>Lorem ipsum dolor sit amet.</small></p>
+                <p class="card-text"><?php echo $row->description ?></p>
+                <p class="card-text"><small><?php echo $row->shipping_cost ?></small></p>
+                <p class="card-text"><small><?php echo $row->location ?></small></p>
               </div>
               
             </div>
           </div>
         </div>
-        <div class="card mb-2 ">
+        <?php 
+         }
+          
+        }
+      }
+        ?>
+        <!-- <div class="card mb-2 ">
           <div class="row">
             <div class="col-auto">
               <img src="https://picsum.photos/200" alt="">
@@ -174,7 +270,7 @@
               
             </div>
           </div>
-        </div>
+        <!-- </div>
         
         <div class="card mb-2 ">
           <div class="row">
@@ -214,7 +310,7 @@
               
             </div>
           </div>
-        </div>
+        </div> -->
 
       </section>
     </div>
