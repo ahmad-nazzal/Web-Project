7629c33e55108a1105794e15dc27ff505b775c4b
SALAHT4N <salah.aldin.tanbour@outlook.com>
2022-12-17 00:03:48 +0200

    before checkout

--- html/add_item.php
+++ html/add_item.php
@@ -3,6 +3,7 @@ include_once("../php_duplicate_code/stylesheets_import.php");
 include_once("../php_duplicate_code/classes/card.php");
 include_once("toast.php");
 include_once("database.php");
+include_once("../php_duplicate_code/classes/add_item_template.php");
 // 780px best width
 
 
@@ -49,13 +50,15 @@ function get_item_object_from_database($item_id)
 
 $item_obj = 0;
 $item_imgs = 0;
+$create_flag = true;
 if ($item_id == 0) {
-  $create_flag = false;
+  global $create_flag;
+  $create_flag = true;
 } else {
   global $item_obj;
   global $item_imgs;
 
-  $create_flag = true;
+  $create_flag = false;
   $item_obj = get_item_object_from_database($item_id);
   $item_imgs_result = get_item_images_from_database($item_id);
 
@@ -181,189 +184,15 @@ if ($item_id == 0) {
 
 
   <div class="container ">
-    <h1 class="mt-5">تعديل/انشاء </h1>
-    <section class="section-images">
-      <h2 class="text-center heading-helper">الصور</h2>
-      <div class="img-display text-center mb-5">
-        <?php
-        if (!$create_flag) {
-        } else {
 
-
-        ?>
-          <img src="<?php echo $item_images[0]; ?>" alt="image" id="big-img">
-        <?php
-        }
-        ?>
-      </div>
-      <form action="my_items_page.php" method="post" id="main-form">
-
-        <!-- <!?php renderToast(); ?> -->
-        <input type="text" name="create" value="<?php echo $item_id; ?>" style="display: none;">
-        <!-- <label for="file" style="cursor: pointer;">اضف صور</label> -->
-
-
-
-
-
-        <input class="form-control mb-2" type="file" id="formFileMultiple" onchange="loadFile(event)" multiple>
-        <span>
-
-          <div class="available-imgs">
-            <?php
-            for ($i = 0; $i < count($item_images); $i++) {
-            ?>
-              <img onclick="selectPhoto(this)" src="<?php echo $item_images[$i]; ?>" alt="image" class="item-img">
-            <?php
-            } ?>
-
-          </div>
-        </span>
-    </section>
-
-    <section class="section-info">
-      <h2 class="text-center heading-helper">المعلومات</h2>
-      <h5 class="mb-3">العنوان</h5>
-      <div class="form-group mb-3">
-
-        <input id="title-input" class="form-control" type="text" value="<?php echo $item_obj->Title; ?>">
-      </div>
-      <h5 class="text-description mb-e">الوصف</h5>
-      <div class="form-group">
-        <textarea form="main-form" class="form-control" id="exampleFormControlTextarea1" rows="10"><?php echo $item_obj->Description; ?></textarea>
-      </div>
-    </section>
-    <section class="section-accordion">
-      <h2 class="text-center heading-helper">الخصائص</h2>
-      <div class="accordion" id="accordionPanelsStayOpenExample">
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-              الدفع
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-            <div class="accordion-body">
-              <div class="price">
-                <h3 class=" d-inline"><strong contenteditable="true"><?php echo $item_obj->price_per_day; ?></strong><strong>$</strong></h3>
-                <p class="price-tag d-inline">ليوم واحد</p>
-
-              </div>
-              <div class="form-check w-50">
-                <?php
-                if ($item_obj->cash_method == 1) {
-
-
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked" name="cash-method" checked>
-                <?php
-                } else {
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked" name="cash-method">
-                <?php
-                }
-                ?>
-                <label class="form-check-label" for="flexCheckChecked">
-                  كاش
-                </label>
-
-              </div>
-              <div class="form-check w-50">
-                <?php
-                if ($item_obj->credit_method == 1) {
-
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheck" name="credit-method" checked>
-                <?php
-                } else {
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked" name="credit-method">
-                <?php
-                }
-                ?>
-
-                <label class="form-check-label" for="flexCheck">
-                  بطاقة ائتمان
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-              التوصيل
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-
-            <div class="accordion-body my-2">
-              <div class="form-check w-50">
-                <?php
-                if ($item_obj->local_pickup == 1) {
-
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked" name="self-pickup" checked>
-                <?php
-                } else {
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheckChecked" name="self-pickup">
-                <?php
-                }
-                ?>
-                <label class="form-check-label" for="flexCheckChecked">
-                  استلام شخصي
-                </label>
-              </div>
-              <div class="form-check w-50">
-                <?php
-                if ($item_obj->shipping == 1) {
-
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheck" name="shipping" checked>
-                <?php
-                } else {
-                ?>
-                  <input class="form-check-input" type="checkbox" value="1" id="flexCheck" name="shipping">
-                <?php
-                }
-                ?>
-                <label class="form-check-label" for="flexCheck">
-                  توصيل عادي
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-              الحالة
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-            <div class="accordion-body">
-              <div class="form-check form-switch">
-                <?php
-                if ($item_obj->credit_method == 1) {
-
-                ?>
-                  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-                <?php
-                } else {
-                ?>
-                  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked">
-                <?php
-                }
-                ?>
-                <!-- <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked> -->
-                <label class="form-check-label" for="flexSwitchCheckChecked">متاح</label>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <?php
+    if ($create_flag == false) {
+      (new AddItemTemplate($item_id, $item_images, $item_obj->Title, $item_obj->Description, $item_obj->price_per_day, $item_obj->cash_method, $item_obj->credit_method, $item_obj->local_pickup, $item_obj->shipping, $item_obj->stat))->render();
+    } else {
+      $arr = [];
+      (new AddItemTemplate(null, $arr, "", "", 0, 0, 0, 0, 0, 1))->render();
+    }
+    ?>
     <div class="d-flex justify-content-center">
 
       <button type="submit" class="btn btn-primary my-4 col-4" id="update-btn" onclick="storeItem()">حفظ</button>
